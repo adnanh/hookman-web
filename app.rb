89826e857350db6ee14ab8e2eaeea26e0ced5e54
Adnan Hajdarevic <adnanh@gmail.com>
2015-10-25 13:00:01 +0100

    styling

--- app.rb
+++ app.rb
@@ -1,8 +1,7 @@
 require 'sinatra'
 require 'tilt/erubis'
 require 'yaml'
-
-enable :sessions
+require 'json'
 
 set :config, YAML.load_file('config.yml')
 
@@ -10,6 +9,11 @@ set :bind, settings.config['server']['host'] || '127.0.0.1'
 set :port, settings.config['server']['port'] || 9999
 
 set :version, '0.0.1'
+
+use Rack::Session::Cookie, :key => settings.config['server']['cookie']['name'],
+                           :path => settings.config['server']['cookie']['path'],
+                           :expire_after => settings.config['server']['cookie']['expire_after'],
+                           :secret => settings.config['server']['cookie']['secret']
 
 helpers do
 	def protected!
@@ -21,21 +25,42 @@ helpers do
 	def authenticated?
 		!session[:logged_in].nil? && session[:logged_in]
 	end
+
+	def flash(value=nil)
+		if value.nil?
+			session.delete :flash
+		else
+			session[:flash] = value
+		end
+	end
+
 end
 
 get '/' do 
 	protected!
 
-	erb :index, locals: { test: "welcome" }
+	hooks = JSON.parse(File.read(settings.config['webhook']['hooks']))
+
+	erb :index, locals: { hooks: hooks }
+end
+
+get '/log' do 
+	protected!
+
+	log = File.readlines(settings.config['webhook']['log'])
+
+	erb :log, locals: { log: log }
 end
 
 get '/login' do
-	erb :login
+	erb :login, locals: { message: flash }
 end
 
 post '/login' do
 	if params[:username] == settings.config['auth']['username'] && params[:password] == settings.config['auth']['password']
 		session[:logged_in] = true
+	else
+		flash '<b class="error">Invalid username/password combination.</b>'
 	end
 
 	redirect '/'
